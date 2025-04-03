42a3cce92478cd616b8e74e297b05222698723c8
Roei <roei922@gmail.com>
2025-04-03 18:56:44 +0300

    fixed missing imports

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime, timedelta
 
 class Token(BaseModel):
     access_token: str
